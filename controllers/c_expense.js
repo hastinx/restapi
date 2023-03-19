ebe6f419252db35cbd7b7b3790988759f7d12dc7
MuhammadHastin <hastinre@gmail.com>
2023-03-19 08:05:52 +0700

    expense api 1

--- controllers/c_expense.js
+++ controllers/c_expense.js
@@ -1,15 +1,10 @@
-import { getDataExpense, setExpense, updateExpense } from "../utils/expense.js"
 import { action } from "../utils/action.js"
-import { formatDate } from "../utils/formatDate.js"
-
 
 
 export const getExpense = (req, res) => {
-    console.log('======================================================================== GET ========================================================================')
     const { id, startdate, enddate, category } = req.query
-
     if (id) {
-        res.json(action.getExpenseById)
+        res.json(action.getExpenseById(id))
     } else if (startdate && enddate) {
         res.json(action.getExpenseByDate(startdate, enddate))
     } else if (category) {
@@ -17,27 +12,18 @@ export const getExpense = (req, res) => {
     } else {
         res.json(action.getExpense())
     }
-
 }
 
 export const postExpense = (req, res) => {
-    console.log('======================================================================== POST ========================================================================')
-
     res.json(action.postExpense(req.body))
-
 }
 
 export const editExpense = (req, res) => {
-    console.log('======================================================================== PATCH ID ' + req.body.id + ' ========================================================================')
-
     const { id, name, nominal, category } = req.body
     res.json(action.editExpense(id, name, nominal, category))
-
 }
 
 export const deleteExpense = (req, res) => {
-    console.log('======================================================================== DELETE ID ' + req.params.id + ' ========================================================================')
-
     let id = Number(req.params.id)
     res.json(action.deleteExpense(id))
 }
--- utils/action.js
+++ utils/action.js
@@ -1,15 +1,18 @@
 import { getDataExpense, setExpense, updateExpense } from "./expense.js"
+import { formatDate } from "./formatDate.js"
 
 
 let expense = JSON.parse(getDataExpense)
 export const action = {
     getExpense() {
+        console.log('======================================================================== GET ========================================================================')
         return {
             status: 200,
             values: expense
         }
     },
     getExpenseByDate(start, end) {
+        console.log('======================================================================== GET BY DATE ========================================================================')
         let found = expense.filter(i => new Date(i.created_at) >= new Date(start) && new Date(i.created_at) <= new Date(end))
         let sum = 0
         found.map(i => sum += i.nominal)
@@ -30,7 +33,9 @@ export const action = {
 
     },
     getExpenseByCategory(category) {
+        console.log('======================================================================== GET BY CATEGORY ========================================================================')
         let found = expense.filter(i => (i.category).toLowerCase() === category.toLowerCase())
+        console.log(found)
         let sum = 0
         found.map(i => sum += i.nominal)
         if (found.length !== 0) {
@@ -49,28 +54,48 @@ export const action = {
         }
     },
     getExpenseById(id) {
+        console.log('======================================================================== GET BY ID ========================================================================')
         let found = expense.find(i => i.id === Number(id))
         let expenseById = []
         expenseById.push(found)
-        return {
-            status: 200,
-            values: expenseById
+        if (found !== undefined) {
+            return {
+                status: 200,
+                values: expenseById
+            }
+        } else {
+            return {
+                status: 404,
+                message: "Data not found"
+            }
         }
     },
     postExpense(data) {
+        console.log('======================================================================== POST ========================================================================')
+        let found = expense.find(i => (i.name).toLowerCase() === data.name.toLowerCase())
+        console.log(found)
         const newId = expense.length + 1
         const newExpense = {
             id: newId,
             ...data,
             created_at: formatDate(Date.now()),
         }
-        setExpense(JSON.stringify(newExpense))
-        return {
-            status: 200,
-            message: "Data insert success"
+
+        if (found === undefined) {
+            setExpense(JSON.stringify(newExpense))
+            return {
+                status: 200,
+                message: "Data insert success"
+            }
+        } else {
+            return {
+                status: 405,
+                message: "Data already exist"
+            }
         }
     },
     editExpense(id, name, nominal, category) {
+        console.log('======================================================================== PATCH ========================================================================')
         let index = expense.findIndex((i) => i.id === id);
         let found = expense.find(i => i.id === id)
         if (name !== undefined) found.name = name;
@@ -80,36 +105,40 @@ export const action = {
 
         expense.splice(index, 1, found)
 
-        updateExpense(JSON.stringify(expense))
-        if (index === 1) {
-            return {
-                status: 201,
-                message: "Data update success"
-            }
-        } else {
+
+        if (index === -1) {
             return {
                 status: 404,
                 message: "Data not found"
+            }
+        } else {
+            updateExpense(JSON.stringify(expense))
+            return {
+                status: 201,
+                message: "Data update success"
             }
         }
 
     },
     deleteExpense(id) {
+        console.log('======================================================================== DELETE ========================================================================')
         let deleteExpense = JSON.parse(getDataExpense)
         let index = deleteExpense.findIndex((i) => i.id === id);
 
         deleteExpense.splice(index, 1)
         console.log(deleteExpense)
-        updateExpense(JSON.stringify(deleteExpense))
-        if (index == 1) {
-            return {
-                status: 200,
-                message: "Data delete success"
-            }
-        } else {
+
+        if (index === -1) {
             return {
                 status: 404,
                 message: "Data not found"
+            }
+
+        } else {
+            updateExpense(JSON.stringify(deleteExpense))
+            return {
+                status: 200,
+                message: "Data delete success"
             }
         }
 
